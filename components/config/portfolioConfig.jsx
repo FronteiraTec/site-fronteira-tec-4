6e910d4dd6c395c21a645db1d3cf72cf2ef33e9f
HochBr <gustavo.botezini@gmail.com>
2026-01-21 14:32:06 -0300

    Configure custom domain fronteiratec.com

--- components/config/portfolioConfig.jsx
+++ components/config/portfolioConfig.jsx
@@ -15,7 +15,7 @@ export const projects = [
     title: 'Site SemFronteiras',
     category: 'Web', // Deve ser uma das categorias acima
     description: 'Site institucional para a Empresa Júnior SemFronteiras, focado em ofertar serviços de consultoria empresarial.',
-    image: '/site-fronteira-tec-4/imagens/SemFronteiras.png',
+    image: '/imagens/SemFronteiras.png',
     tech: ['React', 'HTML', 'Tailwind CSS', 'JavaScript']
   },
 
@@ -24,7 +24,7 @@ export const projects = [
     title: 'Sistema da Incubadora de Negócios',
     category: 'Sistemas',
     description: 'Sistema integrado para a gestão dos incubados.',
-    image: '/site-fronteira-tec-4/imagens/inne.png',
+    image: '/imagens/inne.png',
     tech: ['React.js', 'Node.js', 'PostgreSQL']
   },
 
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next";
 
 const isProduction = process.env.NODE_ENV === 'production';
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProduction ? '/site-fronteira-tec-4' : '',
+  basePath: isProduction && isGitHubPages ? '/site-fronteira-tec-4' : '',
   images: {
     unoptimized: true
   }
